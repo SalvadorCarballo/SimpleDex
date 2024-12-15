3c60ab080fd92bc3cb6f97b6e9cd19d394f4380f
SalvadorCarballo <salvador.carballo@gmail.com>
2024-12-15 18:17:59 +0000

    Clase 3 - 1era parte - C. Marchese

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const AlchemyUrl = process.env.ALCHEMY_URL;
+const prKey = process.env.PRIVATE_KEY;
+const ethScanApi = process.env.ETHERSCAN_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: AlchemyUrl,
+      accounts: [prKey],
+      },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ethScanApi,
+    }
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
